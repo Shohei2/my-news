791f6647745da6ef6b0f041c9dd4b65293624770
EC2 Default User <ec2-user@ip-172-31-23-124.us-east-2.compute.internal>
2021-04-26 12:09:01 +0000

    課題提出

---
+++ app/History.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class History extends Model
+{
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'news_id' => 'required',
+        'edited_at' => 'required',
+    );
+    /// 以下を追記
+    // News Modelに関連付けを行う
+    public function histories()
+    {
+      return $this->hasMany('App\History');
+
+    }
+}
